16c1469e39ecd4ac56fb169d8de71505c887a4ab
Wloner0809 <wloner@mail.ustc.edu.cn>
2023-05-12 21:40:20 +0800

    线程池基本完成

--- lab3/src/server.c
+++ lab3/src/server.c
@@ -17,26 +17,24 @@
 #define MAX_SEND_LEN 1048576
 // 路径长度最大为4096
 #define MAX_PATH_LEN 4096
-#define MAX_HOST_LEN 1024
 #define MAX_CONN 1024
 
 #define HTTP_STATUS_200 "200 OK"
 #define HTTP_STATUS_404 "404 Not Found"
 #define HTTP_STATUS_500 "500 Internal Server Error"
 
-//任务队列的大小
+// 任务队列的大小
 #define QUEUE_SIZE 40960
-//线程数目
+// 线程数目
 #define THREAD_NUM 100
 
 typedef struct
 {
     pthread_mutex_t mutex;
 
-    // 三个条件变量
+    // 两个条件变量
     pthread_cond_t task_queue_not_full;
     pthread_cond_t task_queue_not_empty;
-    pthread_cond_t task_queue_empty;
 
     int *task_queue;
     int task_queue_front;
@@ -45,8 +43,6 @@ typedef struct
 
     int thread_num; // 线程池中开启的线程数
     pthread_t *threads;
-
-    int shutdown_pool; // 标志线程池的使用状态
 
 } threadpool;
 
@@ -90,6 +86,19 @@ int parse_request(char *request, char *path)
 
     memcpy(path, req + 3, (index - 2) * sizeof(char));
     path[index - 3] = '\0';
+
+    // for(int i = 0; i < strlen("HTTP/1.0\r\nHost: 127.0.0.1:8000"); i++)
+    // {
+    //     printf("%c",req[index + i + 1]);
+    // }
+    // printf("\n");
+
+    if (strncmp(req + index + 1, "HTTP/1.0\r\nHost: 127.0.0.1:8000\r\n", strlen("HTTP/1.0\r\nHost: 127.0.0.1:8000\r\n")) != 0 && strncmp(req + index + 1, "HTTP/1.1\r\nHost: 127.0.0.1:8000\r\n", strlen("HTTP/1.1\r\nHost: 127.0.0.1:8000\r\n")) != 0)
+    {
+        //请求头不完整
+        return -1;
+    }
+
     return 0;
 }
 
@@ -120,6 +129,9 @@ void handle_clnt(int clnt_sock)
     char *response_tmp = response;
     // 分析文件
     struct stat buf;
+    // 标志变量
+    // 用于标志第一次循环
+    int sig_for_while_loop = 0;
 
     while (1)
     {
@@ -127,36 +139,43 @@ void handle_clnt(int clnt_sock)
             sol_error("reading clnt_sock fails!\n");
         buffer[len] = '\0';
         strcat(req_buf, buffer);
+
+        if (sig_for_while_loop == 0)
+        {
+            // 处理POST请求
+            if (strlen(buffer) < 5 || strncmp(buffer, "GET /", 5) != 0)
+            {
+                // 请求method不是GET
+                sprintf(response, "HTTP/1.0 %s\r\nContent-Length: %zd\r\n\r\n", HTTP_STATUS_500, (ssize_t)0);
+                ssize_t response_len = strlen(response);
+                ssize_t write_len = 0;
+                while (response_len > 0)
+                {
+                    // 通过clnt_sock向客户端发送信息
+                    // 将clnt_sock作为文件描述符写内容
+                    if ((write_len = write(clnt_sock, response, response_len)) < 0)
+                    {
+                        sol_error("writing clnt_sock fails!\n");
+                    }
+                    response = response + write_len;
+                    response_len = response_len - write_len;
+                }
+                // 关闭客户端套接字
+                close(clnt_sock);
+                // 释放内存
+                free(req_buf_tmp);
+                free(buffer_tmp);
+                free(response_tmp);
+                return;
+            }
+        }
+
         if (!strcmp(buffer + strlen(buffer) - 4, "\r\n\r\n"))
             break;
+        sig_for_while_loop = 1;
     }
+    sig_for_while_loop = 0;
     req_len = strlen(req_buf);
-
-    if (req_len < 5 || strncmp(req_buf, "GET /", 5) != 0)
-    {
-        // 请求不完整/请求method不是GET
-        sprintf(response, "HTTP/1.0 %s\r\nContent-Length: %zd\r\n\r\n", HTTP_STATUS_500, (ssize_t)0);
-        ssize_t response_len = strlen(response);
-        ssize_t write_len = 0;
-        while (response_len > 0)
-        {
-            // 通过clnt_sock向客户端发送信息
-            // 将clnt_sock作为文件描述符写内容
-            if ((write_len = write(clnt_sock, response, response_len)) < 0)
-            {
-                sol_error("writing clnt_sock fails!\n");
-            }
-            response = response + write_len;
-            response_len = response_len - write_len;
-        }
-        // 关闭客户端套接字
-        close(clnt_sock);
-        // 释放内存
-        free(req_buf_tmp);
-        free(buffer_tmp);
-        free(response_tmp);
-        return;
-    }
 
     // 根据HTTP请求的内容，解析资源路径和Host头
     char *path = (char *)malloc(MAX_PATH_LEN * sizeof(char));
@@ -257,7 +276,7 @@ void handle_clnt(int clnt_sock)
             if (read_file == NULL)
                 sol_error("malloc fails!\n");
             char *read_file_tmp = read_file;
-            if(read(fd, read_file, buf.st_size) < 0)
+            if (read(fd, read_file, buf.st_size) < 0)
                 sol_error("read file fails!\n");
 
             response_len = strlen(read_file);
@@ -288,14 +307,6 @@ void *threadpool_func(void *thread_pool)
     {
         pthread_mutex_lock(&(pool->mutex));
 
-        // 如果线程池关闭，退出
-        if (pool->shutdown_pool == 1)
-        {
-            pthread_mutex_unlock(&(pool->mutex));
-            // 显式终止线程
-            pthread_exit(NULL);
-        }
-
         // 如果队列为空，则等待队列为非空
         while (pool->task_queue_cur_size == 0)
             pthread_cond_wait(&(pool->task_queue_not_empty), &(pool->mutex));
@@ -309,9 +320,6 @@ void *threadpool_func(void *thread_pool)
             // 队列由满变为不满
             // 可以有新的task加入
             pthread_cond_broadcast(&(pool->task_queue_not_full));
-
-        // if (pool->task_queue_cur_size == 0)
-        //     pthread_cond_signal(&(pool->task_queue_empty));
 
         pthread_mutex_unlock(&(pool->mutex));
 
@@ -335,8 +343,6 @@ threadpool *threadpool_create(int thread_num, int queue_size)
         sol_error("init task_queue_not_full fails!\n");
     if (pthread_cond_init(&(pool->task_queue_not_empty), NULL))
         sol_error("init task_queue_not_empty fails!\n");
-    if (pthread_cond_init(&(pool->task_queue_empty), NULL))
-        sol_error("init task_queue_empty fails!\n");
 
     pool->task_queue = (int *)malloc(sizeof(int) * queue_size);
     if (pool->task_queue == NULL)
@@ -350,8 +356,6 @@ threadpool *threadpool_create(int thread_num, int queue_size)
     if (pool->threads == NULL)
         sol_error("malloc fails!\n");
 
-    pool->shutdown_pool = 0;
-
     for (int i = 0; i < thread_num; i++)
         pthread_create(&(pool->threads[i]), NULL, threadpool_func, (void *)pool);
 
@@ -361,13 +365,6 @@ threadpool *threadpool_create(int thread_num, int queue_size)
 void threadpool_add_a_task(int clnt_sock, threadpool *pool)
 {
     pthread_mutex_lock(&(pool->mutex));
-
-    if (pool->shutdown_pool == 1)
-    {
-        pthread_mutex_unlock(&(pool->mutex));
-        // 显式终止线程
-        pthread_exit(NULL);
-    }
 
     while (pool->task_queue_cur_size == (QUEUE_SIZE - 1))
         // 队列满
@@ -392,6 +389,8 @@ int main()
     // SOCK_STREAM: 面向连接的数据传输方式
     // IPPROTO_TCP: 使用TCP协议
     int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serv_sock == -1)
+        sol_error("creating socket fails!\n");
 
     // 创建线程池
     threadpool *pool = threadpool_create(THREAD_NUM, QUEUE_SIZE);
@@ -410,7 +409,9 @@ int main()
     bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
     // 使得serv_sock套接字进入监听状态，开始等待客户端发起请求
-    listen(serv_sock, MAX_CONN);
+    int listen_test = listen(serv_sock, MAX_CONN);
+    if (listen_test == -1)
+        sol_error("listen fails!\n");
 
     // 接收客户端请求，获得一个可以与客户端通信的新的生成的套接字clnt_sock
     struct sockaddr_in clnt_addr;
